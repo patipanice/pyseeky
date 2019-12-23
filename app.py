ed276b9af05f3a1ddaf38c5836dfb96d71d73b55
patipanice <56648202+patipanice@users.noreply.github.com>
2019-12-23 23:55:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def predict():
 		return data[predicteds]
 		
 # run the app.
-if __name__ == "__main__":
+#if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    app.run()
+  #app.run()
